c7d14b1f8708609159665abea606eb2c16a6b8c9
ally_MSI <ally.chen@codepulse.com.tw>
2019-07-11 01:04:17 +0800

    set default navbar

--- header.php
+++ header.php
@@ -1,21 +1,57 @@
 <!doctype html>
 <html>
-	<head>
-		<title>Codepulse Demo</title>
-		<meta charset="UTF-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
-        <meta http-equiv="content-type" content="text/html; charset=UTF-8"/>
-        <meta name="description" content="" />
-        <meta name="keywords" content="" />
-        <meta name="author" content="Codepulse" />
-        
-        <link href="assets/images/favicon.png" rel="shortcut icon" type="image/png">
-        
-        <link href="assets/css/animate.css" rel="stylesheet" type="text/css">
-        <link href="assets/css/bootstrap.min.css" rel="stylesheet" type="text/css">
-        <link href="assets/css/fontawesome.all.min.css" rel="stylesheet" type="text/css">
-        <link href="assets/css/slick.css" rel="stylesheet" type="text/css">
-        <link href="assets/css/common.css" rel="stylesheet" type="text/css">
-	</head>
 
-	<body>
+<head>
+    <title>Codepulse Demo</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+    <meta name="description" content="" />
+    <meta name="keywords" content="" />
+    <meta name="author" content="Codepulse" />
+
+    <link href="assets/images/favicon.png" rel="shortcut icon" type="image/png">
+
+    <link href="assets/css/animate.css" rel="stylesheet" type="text/css">
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+    <link href="assets/css/fontawesome.all.min.css" rel="stylesheet" type="text/css">
+    <link href="assets/css/slick.css" rel="stylesheet" type="text/css">
+    <link href="assets/css/common.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+    <div class="headerwrap fixed">
+        <header>
+            <div class="container">
+            <nav class="navbar navbar-expand-md">
+                <a class="navbar-brand" href="#">Navbar</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar">
+                    <span class="navbar-toggler-icon"><i class="fas fa-bars"></i></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbar">
+                    <ul class="navbar-nav mr-0 ml-auto">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="#">首頁</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">關於我們</a>
+                        </li>
+                        <li class="nav-item has_submenu">
+                            <a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown">
+                                產品
+                            </a>
+                            <ul>
+                                <li><a class="dropdown-item" href="#">類別1</a></li>
+                                <li><a class="dropdown-item" href="#">類別2</a></li>
+                            </ul>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">最新消息</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+           </div>
+        </header>
+    </div>
--- index.php
+++ index.php
@@ -1,3 +1,4 @@
 <?php include('header.php'); ?>
-<div class="pagecont"><i class="playbar"><span></span></i></div>
+<div class="pagecont">
+</div>
 <?php include('footer.php'); ?>
